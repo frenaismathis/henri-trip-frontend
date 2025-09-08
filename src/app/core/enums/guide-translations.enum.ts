7f9aedda701a3eb5d5dc575a993f3a072253ef88
Mathis Frénais <frenais.mathis@gmail.com>
2025-09-08 18:49:12 +0200

    feat(core): add core services, guards and interceptors

---
+++ src/app/core/enums/guide-translations.enum.ts
@@ -0,0 +1,26 @@
+export enum GuideTranslations {
+    // Mobilities
+    WALK = 'À pied',
+    CAR = 'Voiture',
+    BIKE = 'Vélo',
+    BUS = 'Bus',
+    TRAIN = 'Train',
+    MOTORBIKE = 'Moto',
+
+    // Seasons
+    SPRING = 'Printemps',
+    SUMMER = 'Été',
+    AUTUMN = 'Automne',
+    WINTER = 'Hiver',
+
+    // Publics
+    FAMILY = 'Famille',
+    ADULTS = 'Adultes',
+    CHILDREN = 'Enfants',
+    TEENS = 'Adolescents',
+    SENIORS = 'Seniors',
+    LONELY = 'Seul',
+    COUPLE = 'En couple',
+    FRIENDS = 'Entre amis',
+    ALL = 'Tous'
+}
