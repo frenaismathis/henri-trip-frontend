aa4c1870c104e3b72813702ee19b39cd277a929f
Mathis Frénais <frenais.mathis@gmail.com>
2025-09-08 18:57:54 +0200

    feat(shared): add models and navbar component

---
+++ src/app/shared/models/activity.model.ts
@@ -0,0 +1,12 @@
+export interface Activity {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    address: string;
+    phone: string;
+    openingHours: string;
+    website: string;
+    dayNumber: number;
+    orderInDay: number;
+}
